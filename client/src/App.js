2f5e6eab9dc2b98a1a0f1030cf6cf516ad58e5de
Mindaugas Vosylius <mindaugas.vosylius@gmail.com>
2023-10-23 14:58:58 +0300

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Test
-      </header>
-    </div>
-  );
+import {Component, useState} from 'react';
+
+class App extends Component {
+  state = {
+    clients: []
+  };
+
+  async componentDidMount() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: 'Player 1', 'points': 0, cards: [] })
+    };
+
+    const response = await fetch('/game/create', requestOptions);
+    const body = await response.json();
+    this.setState({clients: body});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Test
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
